ac270405a4db708ca1b69656cd302d24c606aba3
jonathanfigueroa28 <jonathan.figueroa.h@uni.pe>
2023-06-24 17:23:53 -0500

    añadidos ciertas funciones etapa evento pertenecientes a la interfaz 4

--- src/routes/tareas/conection.js
+++ src/routes/tareas/conection.js
@@ -19,10 +19,34 @@ const last_project = async() => {
   } 
   //pool.end();
 }
+//retorna el ultimo evento actualizado (funcion de apoyo)
+const last_event = async() => {
+  try {
+    const txt='SELECT cod_evento FROM evento ORDER BY cod_evento DESC LIMIT 1';
+    const res = await pool.query(txt);
+    console.log(res);
+    return res; 
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+    throw error;
+  } 
+  //pool.end();
+}
+//retorna el ultimo evento actualizado (funcion de apoyo){REVISARRRRRRRRR}
+const last_etapa = async() => {
+  try {
+    const txt='SELECT cod_evento FROM evento ORDER BY cod_evento DESC LIMIT 1';
+    const res = await pool.query(txt);
+    console.log(res);
+    return res; 
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+    throw error;
+  } 
+  //pool.end();
+}
 
-
-
-//Carga de página con los 4 proyectos que aparecen
+//Carga de página con los 4 proyectos que aparecen (INTERFAZ 1)
 const main_projects = async() => {
   try {
     const txt='SELECT fecha_inicio, fecha_fin, nombre FROM proyecto ORDER BY fecha_inicio LIMIT 4';
@@ -34,7 +58,7 @@ const main_projects = async() => {
     throw error;
   }
 }
-//creacion de proyectos
+//creacion de proyectos (INTERFAZ 1)
 const create_project = async(nombre,description,fecha_inicio,fecha_fin,jefe_proyecto,cod_grupo) => {
   const last =  last_project();
   const number = last.substring(2); 
@@ -46,6 +70,18 @@ const create_project = async(nombre,description,fecha_inicio,fecha_fin,jefe_proy
   console.log(res);
   //pool.end();
 }
-//hola
-const hello = "hola";
+
+
+//Creacion de tarea + asignacion (INTERFAZ 4)
+const create_task = async(cod_user,cod_etapa,comentario,fecha_inicio,fecha_fin,hora_inicio,hora_fin) => {
+  const last =  last_project();
+  const number = last.substring(2); 
+  number = parseInt(number) + 1; 
+  const newcode = "PT" + number; 
+
+  const txt='insert into proyecto values (\''+newcode+'\',\''+nombre+'\',\''+description+'\','+fecha_inicio+'\','+fecha_fin+'\',\'planificado\',\''+jefe_proyecto+'\',\''+cod_grupo+'\')';
+  const res = await pool.query(txt);
+  console.log(res);
+  //pool.end();
+}
 module.exports = { create_project, main_projects , pool};
