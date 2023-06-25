d21ebd4dd35bab32fce9e1a8b4d55b9ff799f925
4xeverburga <everburga@gmail.com>
2023-06-25 12:27:47 -0500

    define methods

--- src/routes/tareas/tareas_api.js
+++ src/routes/tareas/tareas_api.js
@@ -7,6 +7,32 @@ const cors = require('cors');
 const corsOptions = { origin:'*' };
 const app = express();
 
+
+
+// pedir las ultimas 100 proyectos de acuerdo a fecha
 app.get('/api/tareas', (req, res) => {
     res.send({msg: 'GET request to api/tareas'});
 });
+
+
+// pedir las ultimas 100 tareas de acuerdo a proyecto id
+app.get('/api/tareas/proyectos', cors(corsOptions), async (req, res) => {
+    // try {
+    //     const result = await db.query(sql`SELECT fecha_inicio, fecha_fin, nombre FROM proyecto ORDER BY fecha_inicio LIMIT 100`);
+    //     res.json(result);
+    // } catch (error) {
+    //     console.error('Error al obtener los datos:', error);
+    //     throw error;
+    // }
+});
+
+// agregar un nuevo proyecto
+app.post('/api/tareas/add/proyecto', (req, res) => {
+    res.send({msg: 'POST request to api/tareas/add/proyecto'});
+});
+
+// agregar una nueva tarea
+app.post('/api/tareas/add/tarea', (req, res) => {
+    res.send({msg: 'POST request to api/tareas/add/tarea'});
+});
+
