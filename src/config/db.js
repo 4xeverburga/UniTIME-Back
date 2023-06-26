482f56fbcc3bfae930a7ad26c6232f0ecb3b2723
4xeverburga <everburga@gmail.com>
2023-06-26 03:02:33 -0500

    added: etapas route

--- src/config/db.js
+++ src/config/db.js
@@ -7,12 +7,11 @@ require('dotenv').config();
 //     database: 'postgres',
 //     port: '5432'
 // })
-console.log(process.env.DB_PASSWORD);
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'postgres',
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     })
 module.exports = pool;
