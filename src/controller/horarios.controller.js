f67590da98926a45abeea9f14960b2489e150ac2
SebastianSalirrosas <sebastian.salirrosas.a@uni.pe>
2023-06-26 08:20:13 -0500

    agregando los querys que faltaban

--- src/controller/horarios.controller.js
+++ src/controller/horarios.controller.js
@@ -1,3 +1,4 @@
+const { text } = require('body-parser');
 const pool = require('../config/db');
 
 
@@ -20,6 +21,7 @@ const getHorarioById = async (req,res)=>{
   const id = req.params.id;
   const text= 'select * from evento e inner join horario h on (h.cod_horario=e.cod_horario) inner join usuario u on (u.cod_usuario=h.cod_usuario) where u.cod_usuario =\''+idusuario+'\' and h.cod_horario = \''+ id+'\';';
   const response = await pool.query(text);
+  res.json(response.rows);
 };
 
 const getHorarioMember = async (req,res)=>{
@@ -36,10 +38,69 @@ const getPersonalHorarios = async (req,res)=>{
   res.json(response.rows);
 }
 
+const createEvents = async(req,res)=>{
+  const body = req.body;
+  const cod_evento = body.cod_evento;
+  const descripcion_evento = body.descripcion_evento;
+  const hora_fin = body.hora_fin;
+  const hora_inicio = body.hora_inicio;
+  const cod_horario=body.cod_horario;
+  const cod_grupo = body.cod_grupo;
+  const text = 'insert into evento(cod_evento,descripcion_evento,hora_inicio,hora_fin,cod_horario,cod_grupo) values(\''+cod_evento+'\',\''+descripcion_evento+'\',\''+hora_inicio+'\',\''+hora_fin+'\',\''+cod_horario+'\',\''+cod_grupo+'\');';
+  const response = await pool.query(text);
+  res.json({
+    message:'created',
+    data:body
+  });
+}
+
+const modifyEvent = async(req,res)=>{
+  const {id}=req.params;
+  const body = req.body;
+  const hora_inicio=body.hora_inicio;
+  const text='update evento set hora_inicio = \''+hora_inicio+'\' where cod_evento=\''+id+'\'';
+  const response = await pool.query(text);
+  res.json({
+    message:'modify',
+    data:body
+  })
+}
+
+const createAviso = async(req,res)=>{
+  const body = req.body;
+  const cod_aviso=body.cod_aviso;
+  const prioridad=body.prioridad;
+  const contenido=body.contenido;
+  const fecha_aviso=body.fecha_aviso;
+  const cod_evento=body.cod_evento;
+  const text = 'insert into aviso(cod_aviso,prioridad,contenido,fecha_aviso,cod_evento) values(\''+cod_aviso+'\',\''+prioridad+'\',\''+contenido+'\',\''+fecha_aviso+'\',\''+cod_evento+'\');';
+  const response = await pool.query(text);
+  res.json({
+    message:'created aviso',
+    data:body
+  });
+}
+
+const modifyAviso = async(req,res)=>{
+  const {id}=req.params;
+  const body = req.body;
+  const fecha_aviso=body.fecha_aviso;
+  const text ='update aviso set fecha_aviso=\''+fecha_aviso+'\' where cod_aviso=\''+id+'\'';
+  const response = await pool.query(text);
+  res.json({
+    message:'modify aviso',
+    data:body
+  })
+}
+
 module.exports={ 
   getHorarioById,
   getHorarios,
   showEventsGroup,
   getHorarioMember,
-  getPersonalHorarios
+  getPersonalHorarios,
+  createEvents,
+  modifyEvent,
+  createAviso,
+  modifyAviso
 }
